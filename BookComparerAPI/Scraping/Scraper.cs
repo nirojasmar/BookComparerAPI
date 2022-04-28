9ac3470d66befbfa0cf9c43f391e6baa97370298
nirojasm <nirojasmar@gmail.com>
2022-04-28 15:45:55 -0500

    RELEASE

--- BookComparerAPI/Scraping/Scraper.cs
+++ BookComparerAPI/Scraping/Scraper.cs
@@ -16,8 +16,6 @@ namespace BookComparerAPI.Scraping
         {
             _scrapBrowser.IgnoreCookies = true;
             _scrapBrowser.Timeout = TimeSpan.FromMinutes(15);
-            /*_scrapBrowser.Headers["User-Agent"] = "Mozilla/4.0 (Compatible; Windows NT 5.1; MSIE 6.0)" +
-                "(compatible, MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)";*/
             _scrapBrowser.Headers["User-Agent"] = Collection.GetRandomDesktop();
             WebPage _webPage = _scrapBrowser.NavigateToPage(new Uri(URL));
             return _webPage.Html;
