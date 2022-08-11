dff5743749351abf820fbdb84225c2de683d5e94
nirojasm <nirojasmar@gmail.com>
2022-08-11 15:40:31 -0500

    Swagger Documentation

--- BookComparerAPI/Program.cs
+++ BookComparerAPI/Program.cs
@@ -1,6 +1,7 @@
 using BookComparerAPI.Scraping;
 using BookComparerAPI.Services;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 Scraper.GetAmazonBook(); //WARNING: May not be the best way to do this; TODO: Async task
@@ -11,7 +12,26 @@ builder.Services.AddDbContext<BookDataContext>(
     );
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Book Comparer API",
+        Description = "An ASP.NET Core Web API that scrapes books and prices data from Amazon and Buscalibre.com",
+        TermsOfService = new Uri("https://example.com/terms"),
+        Contact = new OpenApiContact
+        {
+            Name = "Contact",
+            Url = new Uri("https://example.com/contact")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "License",
+            Url = new Uri("https://example.com/license")
+        }
+    });
+});
 
 var app = builder.Build();
 
